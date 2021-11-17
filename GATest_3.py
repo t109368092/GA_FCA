8911f9e55fee791b2269166c93c18d86aed359c1
吳明洋 <mingyang@wumingyangs-MacBook-Air.local>
2021-11-18 01:39:11 +0800

    add the third objective

--- GATest_3.py
+++ GATest_3.py
@@ -134,6 +134,146 @@ class GATest:
             nr_buffer = []
             ue_avg_lte = []
             ue_avg_nr = []
+            ue_loss_lte = []
+            ue_delay_lte = []
+            ue_loss_nr = []
+            ue_delay_nr = []
+            for _ in range(self.number_of_genes):
+                lte_buffer.append([])
+                nr_buffer.append([])
+                ue_avg_lte.append(0.0)
+                ue_avg_nr.append(0.0)
+                ue_loss_lte.append([0, 0])
+                ue_delay_lte.append([0, 0])
+                ue_loss_nr.append([0, 0])
+                ue_delay_nr.append([0, 0])
+            ue_max_rate_lte = copy.deepcopy(self.signal_weight_lte)
+            ue_max_rate_nr = copy.deepcopy(self.signal_weight_nr)
+            ue_r_R_lte = copy.deepcopy(ue_max_rate_lte)
+            ue_r_R_nr = copy.deepcopy(ue_max_rate_nr)
+
+            packets_to_nr = np.array(self.packet_count) * nr_ratio[i] / 10
+            packets_to_nr = np.asarray(packets_to_nr, dtype = int)
+            packets_to_lte = np.array(self.packet_count) - packets_to_nr
+            packets_to_nr = packets_to_nr.tolist()
+            packets_to_lte = packets_to_lte.tolist()
+
+            for tti in range(20):
+                resource_request_lte = []
+                resource_request_nr = []
+                ue_priority_lte = []
+                ue_priority_nr = []
+
+                for ue in range(self.number_of_genes):
+                    lte_buffer[ue].append(packets_to_lte[ue])
+                    nr_buffer[ue].append(packets_to_nr[ue])
+
+                    resource_request_lte.append(sum(lte_buffer[ue]) * self.packet_size[ue] / self.signal_weight_lte[ue])
+                    resource_request_nr.append(sum(nr_buffer[ue]) * self.packet_size[ue] / self.signal_weight_nr[ue])
+
+                    ue_loss_lte[ue][1] = ue_loss_lte[ue][1] + packets_to_lte[ue]
+                    ue_loss_nr[ue][1] = ue_loss_nr[ue][1] + packets_to_nr[ue]
+
+                    if ue_avg_lte[ue] != 0.0:
+                        ue_r_R_lte[ue] = float(ue_max_rate_lte[ue]) / ue_avg_lte[ue]
+                    if ue_avg_nr[ue] != 0.0:
+                        ue_r_R_nr[ue] = float(ue_max_rate_nr[ue]) / ue_avg_nr[ue]
+
+                ue_r_R_lte_temp = copy.deepcopy(ue_r_R_lte)
+                ue_r_R_nr_temp = copy.deepcopy(ue_r_R_nr)
+
+                for _ in range(self.number_of_genes):
+                    ue_priority_lte.append(ue_r_R_lte_temp.index(max(ue_r_R_lte_temp)))
+                    ue_r_R_lte_temp[ue_r_R_lte_temp.index(max(ue_r_R_lte_temp))] = 0
+                    ue_priority_nr.append(ue_r_R_nr_temp.index(max(ue_r_R_nr_temp)))
+                    ue_r_R_nr_temp[ue_r_R_nr_temp.index(max(ue_r_R_nr_temp))] = 0
+
+                remain_resource_lte = self.lte_resource
+                remain_resource_nr = self.nr_resource
+                allocate_lte_resource_ue_index = 0
+                allocate_nr_resource_ue_index = 0
+                ue_avg_lte = np.array(ue_avg_lte) * tti
+                ue_avg_nr = np.array(ue_avg_nr) * tti
+                while remain_resource_lte > 0 and allocate_lte_resource_ue_index < self.number_of_genes:
+                    current_ue_lte = ue_priority_lte[allocate_lte_resource_ue_index]
+                    if resource_request_lte[current_ue_lte] <= remain_resource_lte:
+                        for index in range(len(lte_buffer[current_ue_lte])):
+                            ue_delay_lte[current_ue_lte][0] = ue_delay_lte[current_ue_lte][0] + lte_buffer[current_ue_lte][index] * (len(lte_buffer[current_ue_lte]) - 1 - index)
+                        ue_delay_lte[current_ue_lte][1] = ue_delay_lte[current_ue_lte][1] + sum(lte_buffer[current_ue_lte])
+                        ue_avg_lte[current_ue_lte] = ue_avg_lte[current_ue_lte] + sum(lte_buffer[current_ue_lte]) * self.packet_size[current_ue_lte]
+                        remain_resource_lte = remain_resource_lte - resource_request_lte[current_ue_lte]
+                        lte_buffer[current_ue_lte] = []
+
+                    if resource_request_lte[current_ue_lte] > remain_resource_lte:
+                        allocatable_packets = remain_resource_lte * self.signal_weight_lte[current_ue_lte] / self.packet_size[current_ue_lte]
+                        while len(lte_buffer[current_ue_lte]) > 0 and allocatable_packets > lte_buffer[current_ue_lte][0]:
+                            allocatable_packets = allocatable_packets - lte_buffer[current_ue_lte][0]
+                            ue_delay_lte[current_ue_lte][0] = ue_delay_lte[current_ue_lte][0] + lte_buffer[current_ue_lte][0] * (len(lte_buffer[current_ue_lte]) - 1)
+                            ue_delay_lte[current_ue_lte][1] = ue_delay_lte[current_ue_lte][1] + lte_buffer[current_ue_lte][0]
+                            ue_avg_lte[current_ue_lte] = ue_avg_lte[current_ue_lte] + lte_buffer[current_ue_lte][0] * self.packet_size[current_ue_lte]
+                            del lte_buffer[current_ue_lte][0]
+                        remain_resource_lte = 0
+
+                    allocate_lte_resource_ue_index = allocate_lte_resource_ue_index + 1
+                
+                while remain_resource_nr > 0 and allocate_nr_resource_ue_index < self.number_of_genes:
+                    current_ue_nr = ue_priority_nr[allocate_nr_resource_ue_index]
+                    if resource_request_nr[current_ue_nr] <= remain_resource_nr:
+                        for index in range(len(nr_buffer[current_ue_nr])):
+                            ue_delay_nr[current_ue_nr][0] = ue_delay_nr[current_ue_nr][0] + nr_buffer[current_ue_nr][index] * (len(nr_buffer[current_ue_nr]) - 1 - index)
+                        ue_delay_nr[current_ue_nr][1] = ue_delay_nr[current_ue_nr][1] + sum(nr_buffer[current_ue_nr])
+                        ue_avg_nr[current_ue_nr] = ue_avg_nr[current_ue_nr] + sum(nr_buffer[current_ue_nr]) * self.packet_size[current_ue_nr]
+                        remain_resource_nr = remain_resource_nr - resource_request_nr[current_ue_nr]
+                        nr_buffer[current_ue_nr] = []
+
+                    if resource_request_nr[current_ue_nr] > remain_resource_nr:
+                        allocatable_packets = remain_resource_nr * self.signal_weight_nr[current_ue_nr] / self.packet_size[current_ue_nr]
+                        while len(nr_buffer[current_ue_nr]) > 0 and allocatable_packets > nr_buffer[current_ue_nr][0]:
+                            allocatable_packets = allocatable_packets - nr_buffer[current_ue_nr][0]
+                            ue_delay_nr[current_ue_nr][0] = ue_delay_nr[current_ue_nr][0] + nr_buffer[current_ue_nr][0] * (len(nr_buffer[current_ue_nr]) - 1)
+                            ue_delay_nr[current_ue_nr][1] = ue_delay_nr[current_ue_nr][1] + nr_buffer[current_ue_nr][0]
+                            ue_avg_nr[current_ue_nr] = ue_avg_nr[current_ue_nr] + nr_buffer[current_ue_nr][0] * self.packet_size[current_ue_nr]
+                            del nr_buffer[current_ue_nr][0]
+                        remain_resource_nr = 0
+                    
+                    allocate_nr_resource_ue_index = allocate_nr_resource_ue_index + 1
+
+                for ue in range(self.number_of_genes):
+                    while len(lte_buffer[ue]) > self.packet_deadline[ue]:
+                        ue_loss_lte[ue][0] = ue_loss_lte[ue][0] + lte_buffer[ue][0]
+                        del lte_buffer[ue][0]
+                    
+                    while len(nr_buffer[ue]) > self.packet_deadline[ue]:
+                        ue_loss_nr[ue][0] = ue_loss_nr[ue][0] + packets_to_nr[ue]
+                        del nr_buffer[ue][0]
+                        
+                ue_avg_lte = ue_avg_lte / (tti + 1)
+                ue_avg_nr = ue_avg_nr / (tti + 1)
+                ue_avg_lte = ue_avg_lte.tolist()
+                ue_avg_nr = ue_avg_nr.tolist()
+
+            print("ue_delay_lte", ue_delay_lte)
+            print("ue_delay_nr", ue_delay_nr)
+            print("ue_loss_lte", ue_loss_lte)
+            print("ue_loss_nr", ue_loss_nr)
+            avg_throughput_lte = sum(ue_avg_lte) / self.number_of_genes
+            avg_throughput_nr = sum(ue_avg_nr) / self.number_of_genes
+            throughput_sum = avg_throughput_lte + avg_throughput_nr
+            fitness.append(throughput_sum)
+
+        return fitness
+
+    def origin_fitness_calculate(self, chromosomes):
+        chromosomes_count = len(chromosomes)
+        nr_ratio = np.array(chromosomes)
+
+        fitness = []
+        for i in range(chromosomes_count):
+            tti = 0
+            lte_buffer = []
+            nr_buffer = []
+            ue_avg_lte = []
+            ue_avg_nr = []
             for _ in range(self.number_of_genes):
                 lte_buffer.append([])
                 nr_buffer.append([])
@@ -150,7 +290,7 @@ class GATest:
             packets_to_nr = packets_to_nr.tolist()
             packets_to_lte = packets_to_lte.tolist()
 
-            for tti in range(10):
+            for tti in range(20):
                 resource_request_lte = []
                 resource_request_nr = []
                 ue_priority_lte = []
